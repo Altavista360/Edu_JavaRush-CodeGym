2536a4e5d462af838d2b50f565922e9ff01d15c5
Altavista360 <github@altavista360.email>
2026-01-16 18:00:26 +0300

    ru.javarush.java.core.level01.task02 | en.codegym.java.core.level01.task02

---
+++ Java/JAVA 25 SELF/tasks/ru.javarush.java.core/level01/task02/Solution.java
@@ -0,0 +1,113 @@
+/**
+ * @author JavaRush's team
+ * @author CodeGym's team
+ * @author Altavista360
+ * @version 1.0.0
+ */
+
+/**
+ * // JavaRush
+ * Задача: Сообщение в чате 💬
+ * @see <a href="https://javarush.com/tasks/ru.javarush.java.core.level01.task02">ru.javarush.java.core.level01.task02</a>
+ */
+
+/**
+ * // CodeGym
+ * Task: Chat message 💬
+ * @see <a href="https://codegym.cc/tasks/en.codegym.java.core.level01.task02">en.codegym.java.core.level01.task02</a>
+ */
+
+/**
+ * // JavaRush
+ * Описание:
+ * Imagine you are sending a message in a chat. Write a program that prints the current year — 2026 — and the emoji 😅. Each message should be on a separate line.
+ */
+
+/**
+ * // CodeGym
+ * Description:
+ * Представьте, что вы отправляете сообщение в чат. Напишите программу, которая выводит на экран текущий год — 2026 — и смайлик 😅. Каждое сообщение должно быть на отдельной строке.
+ */
+
+/**
+ * // JavaRush
+ * Требования:
+ * • Программа должна вывести на экран число 2026.
+ * • Программа должна вывести на экран смайлик 😅.
+ * • Число 2026 и смайлик 😅 должны выводиться на отдельных строках, а не в одной строке.
+ * • Для вывода сообщений на экран необходимо использовать команду System.out.println.
+ */
+
+/**
+ * // CodeGym
+ * Requirements:
+ * • The program must print the number 2026.
+ * • The program must print the emoji 😅.
+ * • The number 2026 and the emoji 😅 must be printed on separate lines, not on the same line.
+ * • Use System.out.println to print messages to the screen.
+ */
+
+/**
+ * // JavaRush
+ * Черновик:
+ * package ru.javarush.java.core.level01.task02;
+ *
+ * public class Solution
+ * {
+ *     public static void main(String[] args)
+ *     {
+ *         // Выводим на экран фразу "Круто быть программистом!😎"
+ *
+ *     }
+ * }
+ */
+
+/**
+ * // CodeGym
+ * Draft:
+ * package en.codegym.java.core.level01.task02;
+ *
+ * public class Solution
+ * {
+ *     public static void main(String[] args)
+ *     {
+ *         // Print the current year 2026 on a separate line
+ *
+ *
+ *         // Print the emoji 😅 on a separate line
+ *
+ *     }
+ * }
+ */
+
+// JavaRush
+package ru.javarush.java.core.level01.task02;
+
+public class Solution
+{
+    public static void main(String[] args)
+    {
+        // Выводим текущий год 2026 на отдельной строке
+        System.out.println("2026");
+
+        // Выводим смайлик 😅 на отдельной строке
+        System.out.println("😅");
+    }
+}
+
+/*
+// CodeGym
+package en.codegym.java.core.level01.task02;
+
+public class Solution
+{
+    public static void main(String[] args)
+    {
+        // Print the current year 2026 on a separate line
+        System.out.println("2026");
+
+        // Print the emoji 😅 on a separate line
+        System.out.println("😅");
+    }
+}
+*/
