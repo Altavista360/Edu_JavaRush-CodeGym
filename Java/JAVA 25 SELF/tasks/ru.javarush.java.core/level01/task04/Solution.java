aa974b12af0437a4921a0d0cd542ca7205ff5a77
Altavista360 <github@altavista360.email>
2026-01-16 18:02:33 +0300

    ru.javarush.java.core.level01.task04 | en.codegym.java.core.level01.task04

---
+++ Java/JAVA 25 SELF/tasks/ru.javarush.java.core/level01/task04/Solution.java
@@ -0,0 +1,113 @@
+/**
+ * @author JavaRush's team
+ * @author CodeGym's team
+ * @author Altavista360
+ * @version 1.0.0
+ */
+
+/**
+ * // JavaRush
+ * Задача: Текущий год 📅
+ * @see <a href="https://javarush.com/tasks/ru.javarush.java.core.level01.task04">ru.javarush.java.core.level01.task04</a>
+ */
+
+/**
+ * // CodeGym
+ * Task: Current Year 📅
+ * @see <a href="https://codegym.cc/tasks/en.codegym.java.core.level01.task04">en.codegym.java.core.level01.task04</a>
+ */
+
+/**
+ * // JavaRush
+ * Описание:
+ * Объявите переменную типа int с именем currentYear. Присвойте ей значение 2026 и выведите это значение на экран.
+ */
+
+/**
+ * // CodeGym
+ * Description:
+ * Declare an int variable named currentYear. Assign it the value 2026 and print this value to the console.
+ */
+
+/**
+ * // JavaRush
+ * Требования:
+ * • В программе должна быть объявлена переменная типа int с именем currentYear.
+ * • Переменной currentYear должно быть присвоено значение 2026.
+ * • Программа должна вывести на экран значение переменной currentYear.
+ */
+
+/**
+ * // CodeGym
+ * Requirements:
+ * • The program must declare an int variable named currentYear.
+ * • The variable currentYear must be assigned the value 2026.
+ * • The program must print the value of the currentYear variable to the console.
+ */
+
+/**
+ * // JavaRush
+ * Черновик:
+ * package ru.javarush.java.core.level01.task04;
+ *
+ * public class Solution
+ * {
+ *     public static void main(String[] args)
+ *     {
+ *         // Объявляем переменную типа int с именем currentYear
+ *
+ *
+ *         // Выводим значение переменной currentYear на экран
+ *
+ *     }
+ * }
+ */
+
+/**
+ * // CodeGym
+ * Draft:
+ * package en.codegym.java.core.level01.task04;
+ *
+ * public class Solution
+ * {
+ *     public static void main(String[] args)
+ *     {
+ *         // Declare an int variable named currentYear
+ *
+ *
+ *         // Print the value of the currentYear variable to the console
+ *
+ *     }
+ * }
+ */
+
+// JavaRush
+package ru.javarush.java.core.level01.task04;
+
+public class Solution
+{
+    public static void main(String[] args)
+    {
+        // Объявляем переменную типа int с именем currentYear
+        int currentYear = 2026;
+        // Выводим значение переменной currentYear на экран
+        System.out.println(currentYear);
+    }
+}
+
+/*
+// CodeGym
+package en.codegym.java.core.level01.task04;
+
+public class Solution
+{
+    public static void main(String[] args)
+    {
+        // Declare an int variable named currentYear
+       int currentYear = 2026;
+
+        // Print the value of the currentYear variable to the console
+        System.out.println(currentYear);
+    }
+}
+*/
