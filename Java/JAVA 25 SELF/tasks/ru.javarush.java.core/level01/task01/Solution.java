b14606ebd6e10188713c4487197a756919c4107f
Altavista360 <github@altavista360.email>
2026-01-16 11:24:27 +0300

    ru.javarush.java.core.level01.task01 | en.codegym.java.core.level01.task01

---
+++ Java/JAVA 25 SELF/tasks/ru.javarush.java.core/level01/task01/Solution.java
@@ -0,0 +1,100 @@
+/**
+ * @author JavaRush's team
+ * @author CodeGym's team
+ * @author Altavista360
+ * @version 1.0.0
+ */
+
+/**
+ * // JavaRush
+ * Задача: Первая команда 😎
+ * @see <a href="https://javarush.com/tasks/ru.javarush.java.core.level01.task01">ru.javarush.java.core.level01.task01</a>
+ */
+
+/**
+ * // CodeGym
+ * Task: First Command 😎
+ * @see <a href="https://codegym.cc/tasks/en.codegym.java.core.level01.task01">en.codegym.java.core.level01.task01</a>
+ */
+
+/**
+ * // JavaRush
+ * Описание:
+ * Напишите программу на Java, которая выводит на экран фразу: "Круто быть программистом!😎"
+ */
+
+/**
+ * // CodeGym
+ * Description:
+ * Write a program in Java that prints to the screen the phrase: "It's cool to be a programmer!😎"
+ */
+
+/**
+ * // JavaRush
+ * Требования:
+ * • Программа должна выводить на экран фразу: Круто быть программистом!😎
+ * • Для вывода текста должно использоваться выражение System.out.println.
+ */
+
+/**
+ * // CodeGym
+ * Requirements:
+ * • The program should print to the screen the phrase: It's cool to be a programmer!😎
+ * • The text must be output using the expression System.out.println.
+ */
+
+/**
+ * // JavaRush
+ * Черновик:
+ * package ru.javarush.java.core.level01.task01;
+ *
+ * public class Solution
+ * {
+ *     public static void main(String[] args)
+ *     {
+ *         // Выводим на экран фразу "Круто быть программистом!😎"
+ *
+ *     }
+ * }
+ */
+
+/**
+ * // CodeGym
+ * Draft:
+ * package en.codegym.java.core.level01.task01;
+ *
+ * public class Solution
+ * {
+ *     public static void main(String[] args)
+ *     {
+ *         // Print to the screen the phrase "It's cool to be a programmer!😎"
+ *
+ *     }
+ * }
+ */
+
+// JavaRush
+package ru.javarush.java.core.level01.task01;
+
+public class Solution
+{
+    public static void main(String[] args)
+    {
+        // Выводим на экран фразу "Круто быть программистом!😎"
+        System.out.println("Круто быть программистом!😎");
+    }
+}
+
+/*
+// CodeGym
+package en.codegym.java.core.level01.task01;
+
+public class Solution
+{
+    public static void main(String[] args)
+    {
+        // Print to the screen the phrase "It's cool to be a programmer!😎"
+        System.out.println("It's cool to be a programmer!😎");
+    }
+}
+*/
