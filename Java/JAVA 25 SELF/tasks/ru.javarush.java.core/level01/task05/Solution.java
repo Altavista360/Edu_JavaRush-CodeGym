0d9becf0ba182aaa2f20266fe19e4c92e23ab57d
Altavista360 <github@altavista360.email>
2026-01-16 18:03:00 +0300

    ru.javarush.java.core.level01.task05 | en.codegym.java.core.level01.task05

---
+++ Java/JAVA 25 SELF/tasks/ru.javarush.java.core/level01/task05/Solution.java
@@ -0,0 +1,116 @@
+/**
+ * @author JavaRush's team
+ * @author CodeGym's team
+ * @author Altavista360
+ * @version 1.0.0
+ */
+
+/**
+ * // JavaRush
+ * Задача: Личное сообщение 💌
+ * @see <a href="https://javarush.com/tasks/ru.javarush.java.core.level01.task05">ru.javarush.java.core.level01.task05</a>
+ */
+
+/**
+ * // CodeGym
+ * Task: Personal message 💌
+ * @see <a href="https://codegym.cc/tasks/en.codegym.java.core.level01.task05">en.codegym.java.core.level01.task05</a>
+ */
+
+/**
+ * // JavaRush
+ * Описание:
+ * Создайте переменную типа String с именем myMessage. Присвойте ей значение "I ❤️ YOU". Выведите это сообщение на экран.
+ */
+
+/**
+ * // CodeGym
+ * Description:
+ * Create a variable of type String named myMessage. Assign it the value "I ❤️ YOU". Print this message to the screen.
+ */
+
+/**
+ * // JavaRush
+ * Требования:
+ * • В программе должна быть объявлена переменная типа String.
+ * • Переменная должна иметь имя myMessage.
+ * • Переменной myMessage должно быть присвоено значение "I ❤️ YOU".
+ * • Значение переменной myMessage должно быть выведено на экран.
+ */
+
+/**
+ * // CodeGym
+ * Requirements:
+ * • The program must declare a variable of type String.
+ * • The variable must be named myMessage.
+ * • The variable myMessage must be assigned the value "I ❤️ YOU".
+ * • The value of the myMessage variable must be printed to the screen.
+ */
+
+/**
+ * // JavaRush
+ * Черновик:
+ * package ru.javarush.java.core.level01.task05;
+ *
+ * public class Solution
+ * {
+ *     public static void main(String[] args)
+ *     {
+ *         // Объявляем переменную типа String с именем myMessage
+ *
+ *
+ *         // Выводим значение переменной myMessage на экран
+ *
+ *     }
+ * }
+ */
+
+/**
+ * // CodeGym
+ * Draft:
+ * package en.codegym.java.core.level01.task05;
+ *
+ * public class Solution
+ * {
+ *     public static void main(String[] args)
+ *     {
+ *         // Declare a variable of type String named myMessage
+ *
+ *
+ *         // Print the value of the myMessage variable to the screen
+ *
+ *     }
+ * }
+ */
+
+// JavaRush
+package ru.javarush.java.core.level01.task05;
+
+public class Solution
+{
+    public static void main(String[] args)
+    {
+        // Объявляем переменную типа String с именем myMessage
+        String myMessage;
+        myMessage = "I ❤️ YOU";
+        // Выводим значение переменной myMessage на экран
+        System.out.println(myMessage);
+    }
+}
+
+/*
+// CodeGym
+package en.codegym.java.core.level01.task05;
+
+public class Solution
+{
+    public static void main(String[] args)
+    {
+        // Declare a variable of type String named myMessage
+        String myMessage;
+        myMessage = "I ❤️ YOU";
+        // Print the value of the myMessage variable to the screen
+        System.out.println(myMessage);
+    }
+}
+*/
