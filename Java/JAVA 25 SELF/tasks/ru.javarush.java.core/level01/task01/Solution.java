b668625dec5bd8e1738f5b0cf753b634c61cba8c
Altavista360 <github@altavista360.email>
2026-01-17 20:54:53 +0300

    ru.javarush.java.core.level01.task01 | en.codegym.java.core.level01.task01

--- Java/JAVA 25 SELF/tasks/ru.javarush.java.core/level01/task01/Solution.java
+++ Java/JAVA 25 SELF/tasks/ru.javarush.java.core/level01/task01/Solution.java
@@ -1,49 +1,47 @@
-/**
- * @author JavaRush's team
- * @author CodeGym's team
- * @author Altavista360
- * @version 1.0.0
+/*
+ * Authors: JavaRush's team, CodeGym's team, Altavista360
+ * Version: 1.0.0
  */
 
-/**
+/*
  * // JavaRush
  * Задача: Первая команда 😎
- * @see <a href="https://javarush.com/tasks/ru.javarush.java.core.level01.task01">ru.javarush.java.core.level01.task01</a>
+ * Ссылка: https://javarush.com/tasks/ru.javarush.java.core.level01.task01
  */
 
-/**
+/*
  * // CodeGym
  * Task: First Command 😎
- * @see <a href="https://codegym.cc/tasks/en.codegym.java.core.level01.task01">en.codegym.java.core.level01.task01</a>
+ * Link: https://codegym.cc/tasks/en.codegym.java.core.level01.task01
  */
 
-/**
+/*
  * // JavaRush
- * Описание:
+ * Условие:
  * Напишите программу на Java, которая выводит на экран фразу: "Круто быть программистом!😎"
  */
 
-/**
+/*
  * // CodeGym
- * Description:
+ * Condition:
  * Write a program in Java that prints to the screen the phrase: "It's cool to be a programmer!😎"
  */
 
-/**
+/*
  * // JavaRush
  * Требования:
  * • Программа должна выводить на экран фразу: Круто быть программистом!😎
  * • Для вывода текста должно использоваться выражение System.out.println.
  */
 
-/**
+/*
  * // CodeGym
  * Requirements:
  * • The program should print to the screen the phrase: It's cool to be a programmer!😎
  * • The text must be output using the expression System.out.println.
  */
 
-/**
+/*
  * // JavaRush
  * Черновик:
  * package ru.javarush.java.core.level01.task01;
@@ -58,7 +56,7 @@
  * }
  */
 
-/**
+/*
  * // CodeGym
  * Draft:
  * package en.codegym.java.core.level01.task01;
@@ -98,3 +96,35 @@ public class Solution
     }
 }
 */
+
+/*
+ * // JavaRush
+ * Правильное решение:
+ * // Author: JavaRush's team
+ * package ru.javarush.java.core.level01.task01;
+ *
+ * public class Solution
+ * {
+ *     public static void main(String[] args)
+ *     {
+ *         // Выводим на экран фразу "Круто быть программистом!😎"
+ *         System.out.println("Круто быть программистом!😎");
+ *     }
+ * }
+ */
+
+/*
+ * // CodeGym
+ * Correct solution:
+ * // Author: CodeGym's team
+ * package en.codegym.java.core.level01.task01;
+ *
+ * public class Solution
+ * {
+ *     public static void main(String[] args)
+ *     {
+ *         // Print to the screen the phrase "It's cool to be a programmer!😎"
+ *         System.out.println("It's cool to be a programmer!😎");
+ *     }
+ * }
+ */
