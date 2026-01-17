b3deba03016df524e28371731119d1a32a152adb
Altavista360 <github@altavista360.email>
2026-01-17 12:34:26 +0300

    ru.javarush.java.core.level01.task10 | en.codegym.java.core.level01.task10

---
+++ Java/JAVA 25 SELF/tasks/ru.javarush.java.core/level01/task10/Solution.java
@@ -0,0 +1,138 @@
+/**
+ * @author JavaRush's team
+ * @author CodeGym's team
+ * @author Altavista360
+ * @version 1.0.0
+ */
+
+/**
+ * // JavaRush
+ * Задача: Заметки для себя 📝
+ * @see <a href="https://javarush.com/tasks/ru.javarush.java.core.level01.task10">ru.javarush.java.core.level01.task10</a>
+ */
+
+/**
+ * // CodeGym
+ * Task: Notes to self 📝
+ * @see <a href="https://codegym.cc/tasks/en.codegym.java.core.level01.task10">en.codegym.java.core.level01.task10</a>
+ */
+
+/**
+ * // JavaRush
+ * Описание:
+ * Представьте, что вы оставляете комментарии в коде для своей команды. Используйте однострочные комментарии, чтобы заблокировать выполнение всех команд в программе. В результате, программа не должна ничего выводить на экран.
+ */
+
+/**
+ * // CodeGym
+ * Description:
+ * Imagine you're leaving comments in the code for your team. Use single-line comments to disable execution of all commands in the program. As a result, the program should not print anything to the console.
+ */
+
+/**
+ * // JavaRush
+ * Требования:
+ * • В коде должны использоваться однострочные комментарии (//) для комментирования строк.
+ * • Все команды в программе должны быть закомментированы с помощью однострочных комментариев.
+ * • В результате выполнения программы не должно быть никакого вывода на экран.
+ */
+
+/**
+ * // CodeGym
+ * Requirements:
+ * • The code must use single-line comments (//) to comment out lines.
+ * • All commands in the program must be commented out using single-line comments.
+ * • The program must not produce any output when executed.
+ */
+
+/**
+ * // JavaRush
+ * Черновик:
+ * package ru.javarush.java.core.level01.task10;
+ *
+ * public class Solution
+ * {
+ *     public static void main(String[] args)
+ *     {
+ *         // Объявляем переменную типа int и присваиваем ей значение 10
+ *         int number = 10;
+ *
+ *         // Объявляем переменную типа String и присваиваем ей значение "Hello, World!"
+ *         String message = "Hello, World!";
+ *
+ *         // Выводим значение переменной number на экран
+ *         System.out.println(number);
+ *
+ *         // Выводим значение переменной message на экран
+ *         System.out.println(message);
+ *     }
+ * }
+ */
+
+/**
+ * // CodeGym
+ * Draft:
+ * package en.codegym.java.core.level01.task10;
+ *
+ * public class Solution
+ * {
+ *     public static void main(String[] args)
+ *     {
+ *         // Declare an int variable and assign it the value 10
+ *         int number = 10;
+ *
+ *         // Declare a String variable and assign it the value "Hello, World!"
+ *         String message = "Hello, World!";
+ *
+ *         // Print the value of the variable number to the console
+ *         System.out.println(number);
+ *
+ *         // Print the value of the variable message to the console
+ *         System.out.println(message);
+ *     }
+ * }
+ */
+
+// JavaRush
+package ru.javarush.java.core.level01.task10;
+
+public class Solution
+{
+    public static void main(String[] args)
+    {
+        // Объявляем переменную типа int и присваиваем ей значение 10
+        //int number = 10;
+
+        // Объявляем переменную типа String и присваиваем ей значение "Hello, World!"
+        //String message = "Hello, World!";
+
+        // Выводим значение переменной number на экран
+        //System.out.println(number);
+
+        // Выводим значение переменной message на экран
+        //System.out.println(message);
+    }
+}
+
+/*
+// CodeGym
+package en.codegym.java.core.level01.task10;
+
+public class Solution
+{
+    public static void main(String[] args)
+    {
+        // Declare an int variable and assign it the value 10
+        //int number = 10;
+
+        // Declare a String variable and assign it the value "Hello, World!"
+        //String message = "Hello, World!";
+
+        // Print the value of the variable number to the console
+        //System.out.println(number);
+
+        // Print the value of the variable message to the console
+        //System.out.println(message);
+    }
+}
+*/
