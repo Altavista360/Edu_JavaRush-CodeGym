6f7b115656453104f93678af47e4efd1c8341e70
Altavista360 <github@altavista360.email>
2026-01-17 20:58:10 +0300

    ru.javarush.java.core.level01.task07 | en.codegym.java.core.level01.task07

--- Java/JAVA 25 SELF/tasks/ru.javarush.java.core/level01/task07/Solution.java
+++ Java/JAVA 25 SELF/tasks/ru.javarush.java.core/level01/task07/Solution.java
@@ -1,37 +1,35 @@
-/**
- * @author JavaRush's team
- * @author CodeGym's team
- * @author Altavista360
- * @version 1.0.0
+/*
+ * Authors: JavaRush's team, CodeGym's team, Altavista360
+ * Version: 1.0.0
  */
 
-/**
+/*
  * // JavaRush
  * Задача: Суммирование результатов в игре 🎮
- * @see <a href="https://javarush.com/tasks/ru.javarush.java.core.level01.task07">ru.javarush.java.core.level01.task07</a>
+ * Ссылка: https://javarush.com/tasks/ru.javarush.java.core.level01.task07
  */
 
-/**
+/*
  * // CodeGym
  * Task: Summing game scores 🎮
- * @see <a href="https://codegym.cc/tasks/en.codegym.java.core.level01.task07">en.codegym.java.core.level01.task07</a>
+ * Link: https://codegym.cc/tasks/en.codegym.java.core.level01.task07
  */
 
-/**
+/*
  * // JavaRush
- * Описание:
+ * Условие:
  * Представьте, что вы ведете счет в игре. Объявите две переменные типа int: player1Score со значением 1999 и player2Score со значением 25.
  * Создайте переменную totalScore, в которую запишите общую сумму очков обоих игроков, и выведите её на экран.
 */
 
-/**
+/*
  * // CodeGym
- * Description:
+ * Condition:
  * Imagine you are keeping score in a game. Declare two variables of type int: player1Score with the value 1999 and player2Score with the value 25.
  * Create a variable totalScore to store the combined score of both players, and print it to the console.
  */
 
-/**
+/*
  * // JavaRush
  * Требования:
  * • В программе должны быть объявлены две переменные типа int: player1Score и player2Score.
@@ -40,7 +38,7 @@
  * • Значение переменной totalScore должно быть выведено на экран.
  */
 
-/**
+/*
  * // CodeGym
  * Requirements:
  * • The program must declare two int variables: player1Score and player2Score.
@@ -49,7 +47,7 @@
  * • The value of totalScore must be printed to the console.
  */
 
-/**
+/*
  * // JavaRush
  * Черновик:
  * package ru.javarush.java.core.level01.task07;
@@ -70,7 +68,7 @@
  * }
  */
 
-/**
+/*
  * // CodeGym
  * Draft:
  * package en.codegym.java.core.level01.task07;
@@ -132,3 +130,49 @@ public class Solution
     }
 }
 */
+
+/*
+ * // JavaRush
+ * Правильное решение:
+ * // Author: JavaRush's team
+ * package ru.javarush.java.core.level01.task07;
+ *
+ * public class Solution
+ * {
+ *     public static void main(String[] args)
+ *     {
+ *         // Объявляем переменные для хранения очков двух игроков и сразу инициализируем их нужными значениями
+ *         int player1Score = 1999;
+ *         int player2Score = 25;
+ *
+ *         // Вычисляем общую сумму очков
+ *         int totalScore = player1Score + player2Score;
+ *
+ *         // Выводим на экран значение общей суммы очков
+ *         System.out.println(totalScore);
+ *     }
+ * }
+ */
+
+/*
+ * // CodeGym
+ * Correct solution:
+ * // Author: CodeGym's team
+ * package en.codegym.java.core.level01.task07;
+ *
+ * public class Solution
+ * {
+ *     public static void main(String[] args)
+ *     {
+ *         // Declare variables to store the two players' scores and initialize them with the required values
+ *         int player1Score = 1999;
+ *         int player2Score = 25;
+ *
+ *         // Calculate the total score
+ *         int totalScore = player1Score + player2Score;
+ *
+ *         // Print the total score to the console
+ *         System.out.println(totalScore);
+ *     }
+ * }
+ */
