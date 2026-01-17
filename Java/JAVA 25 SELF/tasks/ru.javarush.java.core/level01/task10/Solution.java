7f1a5d4bf0a62892d08411774c64235e72b46fdc
Altavista360 <github@altavista360.email>
2026-01-17 20:59:25 +0300

    ru.javarush.java.core.level01.task10 | en.codegym.java.core.level01.task10

--- Java/JAVA 25 SELF/tasks/ru.javarush.java.core/level01/task10/Solution.java
+++ Java/JAVA 25 SELF/tasks/ru.javarush.java.core/level01/task10/Solution.java
@@ -1,35 +1,33 @@
-/**
- * @author JavaRush's team
- * @author CodeGym's team
- * @author Altavista360
- * @version 1.0.0
+/*
+ * Authors: JavaRush's team, CodeGym's team, Altavista360
+ * Version: 1.0.0
  */
 
-/**
+/*
  * // JavaRush
  * Задача: Заметки для себя 📝
- * @see <a href="https://javarush.com/tasks/ru.javarush.java.core.level01.task10">ru.javarush.java.core.level01.task10</a>
+ * Ссылка: https://javarush.com/tasks/ru.javarush.java.core.level01.task10
  */
 
-/**
+/*
  * // CodeGym
  * Task: Notes to self 📝
- * @see <a href="https://codegym.cc/tasks/en.codegym.java.core.level01.task10">en.codegym.java.core.level01.task10</a>
+ * Link: https://codegym.cc/tasks/en.codegym.java.core.level01.task10
  */
 
-/**
+/*
  * // JavaRush
- * Описание:
+ * Условие:
  * Представьте, что вы оставляете комментарии в коде для своей команды. Используйте однострочные комментарии, чтобы заблокировать выполнение всех команд в программе. В результате, программа не должна ничего выводить на экран.
  */
 
-/**
+/*
  * // CodeGym
- * Description:
+ * Condition:
  * Imagine you're leaving comments in the code for your team. Use single-line comments to disable execution of all commands in the program. As a result, the program should not print anything to the console.
  */
 
-/**
+/*
  * // JavaRush
  * Требования:
  * • В коде должны использоваться однострочные комментарии (//) для комментирования строк.
@@ -37,7 +35,7 @@
  * • В результате выполнения программы не должно быть никакого вывода на экран.
  */
 
-/**
+/*
  * // CodeGym
  * Requirements:
  * • The code must use single-line comments (//) to comment out lines.
@@ -45,7 +43,7 @@
  * • The program must not produce any output when executed.
  */
 
-/**
+/*
  * // JavaRush
  * Черновик:
  * package ru.javarush.java.core.level01.task10;
@@ -69,7 +67,7 @@
  * }
  */
 
-/**
+/*
  * // CodeGym
  * Draft:
  * package en.codegym.java.core.level01.task10;
@@ -136,3 +134,53 @@ public class Solution
     }
 }
 */
+
+/*
+ * // JavaRush
+ * Правильное решение:
+ * // Author: JavaRush's team
+ * package ru.javarush.java.core.level01.task10;
+ *
+ * public class Solution
+ * {
+ *     public static void main(String[] args)
+ *     {
+ *         // Объявляем переменную типа int и присваиваем ей значение 10
+ *         // int number = 10;
+ *
+ *         // Объявляем переменную типа String и присваиваем ей значение "Hello, World!"
+ *         // String message = "Hello, World!";
+ *
+ *         // Выводим значение переменной number на экран
+ *         // System.out.println(number);
+ *
+ *         // Выводим значение переменной message на экран
+ *         // System.out.println(message);
+ *     }
+ * }
+ */
+
+/*
+ * // CodeGym
+ * Correct solution:
+ * // Author: CodeGym's team
+ * package en.codegym.java.core.level01.task10;
+ *
+ * public class Solution
+ * {
+ *     public static void main(String[] args)
+ *     {
+ *         // Declare an int variable and assign it the value 10
+ *         // int number = 10;
+ *
+ *         // Declare a String variable and assign it the value "Hello, World!"
+ *         // String message = "Hello, World!";
+ *
+ *         // Print the value of the variable number to the console
+ *         // System.out.println(number);
+ *
+ *         // Print the value of the variable message to the console
+ *         // System.out.println(message);
+ *     }
+ * }
+ */
