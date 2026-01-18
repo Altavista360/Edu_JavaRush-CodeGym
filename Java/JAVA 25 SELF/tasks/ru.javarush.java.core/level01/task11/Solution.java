5580d22e4d9c7b154e50698c52eccbe0b6c3fb18
Altavista360 <github@altavista360.email>
2026-01-18 18:30:40 +0300

    ru.javarush.java.core.level01.task11 | en.codegym.java.core.level01.task11

---
+++ Java/JAVA 25 SELF/tasks/ru.javarush.java.core/level01/task11/Solution.java
@@ -0,0 +1,181 @@
+/*
+ * Authors: JavaRush's team, CodeGym's team, Altavista360
+ * Version: 1.0.0
+ */
+
+/*
+ * // JavaRush
+ * Задача: Блок кода для тестирования 🛠️
+ * Ссылка: https://javarush.com/tasks/ru.javarush.java.core.level01.task11
+ */
+
+/*
+ * // CodeGym
+ * Task: Code block for testing 🛠️
+ * Link: https://codegym.cc/tasks/en.codegym.java.core.level01.task11
+ */
+
+/*
+ * // JavaRush
+ * Условие:
+ * Вы хотите временно отключить большой блок кода, чтобы протестировать другую часть программы. Используйте один многострочный комментарий, чтобы заблокировать все команды в программе. Программа не должна выводить ничего на экран.
+ */
+
+/*
+ * // CodeGym
+ * Condition:
+ * You want to temporarily disable a large block of code to test another part of the program. Use a single multi-line comment to block all statements in the program. The program must not print anything to the screen.
+ */
+
+/*
+ * // JavaRush
+ * • В программе должен быть использован один многострочный комментарий для отключения блока кода.
+ * • Все команды в программе должны быть закомментированы с помощью одного многострочного комментария.
+ * • Программа не должна выводить ничего на экран при запуске.
+ */
+
+/*
+ * // CodeGym
+ * Requirements:
+ * • The program must use a single multi-line comment to disable a block of code.
+ * • All statements in the program must be commented out using a single multi-line comment.
+ * • The program must not print anything to the screen when run.
+ */
+
+/*
+ * // JavaRush
+ * Черновик:
+ * package ru.javarush.java.core.level01.task11;
+ *
+ * public class Solution
+ * {
+ *     public static void main(String[] args)
+ *     {
+ *         // Пример объявления переменной типа int
+ *         int number = 10;
+ *
+ *         // Пример объявления переменной типа String
+ *         String text = "Привет, мир!";
+ *
+ *         // Пример вывода переменных на экран
+ *         System.out.println(number);
+ *         System.out.println(text);
+ *     }
+ * }
+ */
+
+/*
+ * // CodeGym
+ * Draft:
+ * package en.codegym.java.core.level01.task11;
+ *
+ * public class Solution
+ * {
+ *     public static void main(String[] args)
+ *     {
+ *         // Example declaration of an int variable
+ *         int number = 10;
+ *
+ *         // Example declaration of a String variable
+ *         String text = "Hello, world!";
+ *
+ *         // Example of printing variables to the screen
+ *         System.out.println(number);
+ *         System.out.println(text);
+ *     }
+ * }
+ */
+
+// JavaRush
+package ru.javarush.java.core.level01.task11;
+
+public class Solution
+{
+    public static void main(String[] args)
+    {
+        /*
+        // Пример объявления переменной типа int
+        int number = 10;
+
+        // Пример объявления переменной типа String
+        String text = "Привет, мир!";
+
+        // Пример вывода переменных на экран
+        System.out.println(number);
+        System.out.println(text);
+        */
+    }
+}
+
+/*
+// CodeGym
+package en.codegym.java.core.level01.task11;
+
+public class Solution
+{
+    public static void main(String[] args)
+    {
+        /*
+        // Example declaration of an int variable
+        int number = 10;
+
+        // Example declaration of a String variable
+        String text = "Hello, world!";
+
+        // Example of printing variables to the screen
+        System.out.println(number);
+        System.out.println(text);
+        */
+    }
+}
+*/
+
+/*
+ * // JavaRush
+ * Правильное решение:
+ * // Author: JavaRush's team
+ * package ru.javarush.java.core.level01.task11;
+ *
+ * public class Solution
+ * {
+ *     public static void main(String[] args)
+ *     {
+ *         /*
+ *         // Пример объявления переменной типа int
+ *         int number = 10;
+ *
+ *         // Пример объявления переменной типа String
+ *         String text = "Привет, мир!";
+ *
+ *         // Пример вывода переменных на экран
+ *         System.out.println(number);
+ *         System.out.println(text);
+ *         */
+ *     }
+ * }
+ */
+
+/*
+ * // CodeGym
+ * Correct solution:
+ * // Author: CodeGym's team
+ * package en.codegym.java.core.level01.task11;
+ *
+ * public class Solution
+ * {
+ *     public static void main(String[] args)
+ *     {
+ *         /*
+ *         // Example declaration of an int variable
+ *         int number = 10;
+ *
+ *         // Example declaration of a String variable
+ *         String text = "Hello, world!";
+ *
+ *         // Example of printing variables to the screen
+ *         System.out.println(number);
+ *         System.out.println(text);
+ *         */
+ *     }
+ * }
+ */
