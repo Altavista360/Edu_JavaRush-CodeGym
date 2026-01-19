abb0f01a0f7c77df20cb199894a0aadf9200a93b
Altavista360 <github@altavista360.email>
2026-01-19 03:24:06 +0300

    ru.javarush.java.core.level02.task05 | en.codegym.java.core.level02.task05

---
+++ Java/JAVA 25 SELF/tasks/ru.javarush.java.core/level02/task05/Solution.java
@@ -0,0 +1,77 @@
+/*
+ * Authors: JavaRush's team, CodeGym's team, Altavista360
+ * Version: 1.0.0
+ */
+
+/*
+ * // JavaRush
+ * Задача: Сообщение из будущего 👽
+ * Ссылка: https://javarush.com/tasks/ru.javarush.java.core.level02.task05
+ */
+
+/*
+ * // CodeGym
+ * Task: Message from the Future 👽
+ * Link: https://codegym.cc/tasks/en.codegym.java.core.level02.task05
+ */
+
+/*
+ * // JavaRush
+ * Условие:
+ * Представьте, что вы получаете сообщение из будущего. Объявите переменную типа String с именем futureMessage. Присвойте ей значение "Good news everyone!" и выведите это сообщение на экран.
+ */
+
+/*
+ * // JavaRush
+ * Требования:
+ * • В программе должна быть объявлена переменная с именем futureMessage типа String.
+ * • Переменной futureMessage должно быть присвоено значение "Good news everyone!".
+ * • Программа должна вывести на экран содержимое переменной futureMessage.
+ * • Для вывода строки на экран необходимо использовать команду System.out.println.
+ */
+
+/*
+ * // JavaRush
+ * Черновик:
+ * package ru.javarush.java.core.level02.task05;
+ *
+ * public class Solution {
+ *     public static void main(String[] args) {
+ *         // Объявляем строковую переменную и присваиваем ей сообщение из будущего
+ *
+ *
+ *         // Выводим сообщение на экран
+ *
+ *     }
+ * }
+ */
+
+// JavaRush
+package ru.javarush.java.core.level02.task05;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Объявляем строковую переменную и присваиваем ей сообщение из будущего
+        String futureMessage = "Good news everyone!";
+
+        // Выводим сообщение на экран
+        System.out.println(futureMessage);
+    }
+}
+
+/*
+ * // JavaRush
+ * Правильное решение:
+ * // Author: JavaRush's team
+ * package ru.javarush.java.core.level02.task05;
+ *
+ * public class Solution {
+ *     public static void main(String[] args) {
+ *         // Объявляем строковую переменную и присваиваем ей сообщение из будущего
+ *         String futureMessage = "Good news everyone!";
+ *
+ *         // Выводим сообщение на экран
+ *         System.out.println(futureMessage);
+ *     }
+ * }
+ */
