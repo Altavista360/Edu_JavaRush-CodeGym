e970c8af0f09374242714e03eee60ef2d4c260f7
Altavista360 <github@altavista360.email>
2026-01-24 08:48:02 +0300

    ru.javarush.java.core.level03.task09 | en.codegym.java.core.level03.task09

---
+++ Java/JAVA 25 SELF/tasks/ru.javarush.java.core/level03/task09/Solution.java
@@ -0,0 +1,77 @@
+/*
+ * Authors: JavaRush's team, CodeGym's team, Altavista360
+ * Version: 1.0.0
+ */
+
+/*
+ * // JavaRush
+ * Задача: Поиск правды 🧐
+ * Ссылка: https://javarush.com/tasks/ru.javarush.java.core.level03.task09
+ */
+
+/*
+ * // CodeGym
+ * Task: Finding the Truth 🧐
+ * Link: https://codegym.cc/tasks/en.codegym.java.core.level03.task09
+ */
+
+/*
+ * // JavaRush
+ * Условие:
+ * Вы пишете программу, чтобы проверить правдивость утверждения. Создайте переменную типа boolean с именем isJavaFun и присвойте ей значение true. Выведите это значение на экран.
+ */
+
+/*
+ * // JavaRush
+ * Требования:
+ * • В программе должна быть объявлена переменная типа boolean с именем isJavaFun.
+ * • Переменной isJavaFun должно быть присвоено значение true.
+ * • Программа должна вывести на экран значение переменной isJavaFun.
+ * • В коде не должно быть попыток использовать другие типы данных для переменной isJavaFun.
+ */
+
+/*
+ * // JavaRush
+ * Черновик:
+ * package ru.javarush.java.core.level03.task09;
+ *
+ * public class Solution {
+ *     public static void main(String[] args) {
+ *         // Объявляем переменную типа boolean с именем isJavaFun
+ *
+ *
+ *         // Выводим значение переменной isJavaFun на экран
+ *
+ *     }
+ * }
+ */
+
+// JavaRush
+package ru.javarush.java.core.level03.task09;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Объявляем переменную типа boolean с именем isJavaFun
+        boolean isJavaFun = true;
+
+        // Выводим значение переменной isJavaFun на экран
+        System.out.println(isJavaFun);
+    }
+}
+
+/*
+ * // JavaRush
+ * Правильное решение:
+ * // Author: JavaRush's team
+ * package ru.javarush.java.core.level03.task09;
+ *
+ * public class Solution {
+ *     public static void main(String[] args) {
+ *         // Объявляем переменную типа boolean с именем isJavaFun
+ *         boolean isJavaFun = true;
+ *
+ *         // Выводим значение переменной isJavaFun на экран
+ *         System.out.println(isJavaFun);
+ *     }
+ * }
+ */
