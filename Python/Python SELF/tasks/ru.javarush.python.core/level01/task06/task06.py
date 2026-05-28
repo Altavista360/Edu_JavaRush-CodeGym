bc55b2dafb574df58b8abc6e64abadc491bbf6a0
Altavista360 <github@altavista360.email>
2026-05-28 20:13:02 +0300

    ru.javarush.python.core.level01.task06 | en.codegym.python.core.level01.task06

---
+++ Python/Python SELF/tasks/ru.javarush.python.core/level01/task06/task06.py
@@ -0,0 +1,60 @@
+## Authors: JavaRush's team, CodeGym's team, Altavista360
+## Version: 1.0.0
+
+### JavaRush
+## Задача: Welcome
+## Ссылка: https://javarush.com/tasks/ru.javarush.python.core.level01.task06
+
+### CodeGym
+## Task: Welcome
+## Link: https://codegym.cc/tasks/en.codegym.python.core.level01.task06
+
+### JavaRush
+## Условие:
+# Выведите на экран любую понравившуюся вам фразу.
+
+### CodeGym
+## Condition:
+# Display any phrase you like on the screen.
+
+### JavaRush
+## Требования:
+# • Программа должна выводить на экран любой текст.
+# • Для вывода сообщения должна использоваться функция print().
+
+### CodeGym
+## Requirements:
+# • The program should display any text on the screen.
+# • The print() function should be used to display the message.
+
+### JavaRush
+## Черновик:
+# # Welcome
+#
+# # Выведите на экран любую понравившуюся вам фразу.
+#
+# # Напишите тут ваш код
+
+### CodeGym
+## Draft:
+# # Welcome
+#
+# # Display any phrase you like on the screen.
+#
+# # Write your code here
+
+### JavaRush
+print("Hello, World!")
+
+### CodeGym
+# print("Hello, World!")
+
+### JavaRush
+## Правильное решение:
+## Author: JavaRush's team
+# print("Темная сторона — это путь к силе, которая не знает границ.")
+
+### CodeGym
+## Correct solution:
+## Author: CodeGym's team
+# print("The dark side is a path to power that has no limits.")
