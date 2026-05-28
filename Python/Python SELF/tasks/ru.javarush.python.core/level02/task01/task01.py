ad9a8c9392932b017702740614978de7433fa31f
Altavista360 <github@altavista360.email>
2026-05-28 20:13:35 +0300

    ru.javarush.python.core.level02.task01 | en.codegym.python.core.level02.task01

---
+++ Python/Python SELF/tasks/ru.javarush.python.core/level02/task01/task01.py
@@ -0,0 +1,70 @@
+## Authors: JavaRush's team, CodeGym's team, Altavista360
+## Version: 1.0.0
+
+### JavaRush
+## Задача: Welcome
+## Ссылка: https://javarush.com/tasks/ru.javarush.python.core.level02.task01
+
+### CodeGym
+## Task: Welcome
+## Link: https://codegym.cc/tasks/en.codegym.python.core.level02.task01
+
+### JavaRush
+## Условие:
+# Создайте переменную name и присвойте ей ваше имя.
+# Выведите сообщение "Привет, {имя}" используя функцию print() и вашу переменную.
+
+### CodeGym
+## Condition:
+# Create a variable name and assign it your name.
+# Display the message "Hello, {name}" using the print() function and your variable.
+
+### JavaRush
+## Требования:
+# • Программа должна включать переменную name, которой присваивается строковое значение, содержащее имя.
+# • Программа должна выводить на экран сообщение, которое включает имя, хранящееся в переменной name. Формат сообщения: "Привет, {имя}".
+# • Для вывода сообщения должна использоваться функция print().
+
+### CodeGym
+## Requirements:
+# • The program must include a variable name, which is assigned a string value containing the name.
+# • The program should display a message that includes the name stored in the variable name. The format of the message: "Hello, {name}".
+# • The print() function must be used to display the message.
+
+### JavaRush
+## Черновик:
+# # Welcome
+#
+# # Создайте переменную name и присвойте ей ваше имя.
+# # Выведите сообщение "Привет, {имя}" используя функцию print() и вашу переменную.
+#
+# # Напишите тут ваш код
+
+### CodeGym
+## Draft:
+# # Welcome
+#
+# # Create a variable name and assign it your name.
+# # Display the message "Hello, {name}" using the print() function and your variable.
+#
+# # Write your code here
+
+### JavaRush
+name = "John Smith"
+print("Привет,", name)
+
+### CodeGym
+# name = "John Smith"
+# print("Hello,", name)
+
+### JavaRush
+## Правильное решение:
+## Author: JavaRush's team
+# name = "Алексей"  # Замените "Ваше имя" на ваше реальное имя
+# print("Привет,", name)
+
+### CodeGym
+## Correct solution:
+## Author: CodeGym's team
+# name = "Alexey"  # Replace "Your name" with your real name
+# print("Hello,", name)
