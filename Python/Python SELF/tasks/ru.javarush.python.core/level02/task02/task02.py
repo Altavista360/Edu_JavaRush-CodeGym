2268e4e03a0bb4d86ce03a660804de034b6f95e4
Altavista360 <github@altavista360.email>
2026-05-29 16:36:48 +0300

    ru.javarush.python.core.level02.task02 | en.codegym.python.core.level02.task02

---
+++ Python/Python SELF/tasks/ru.javarush.python.core/level02/task02/task02.py
@@ -0,0 +1,80 @@
+## Authors: JavaRush's team, CodeGym's team, Altavista360
+## Version: 1.0.0
+
+### JavaRush
+## Задача: Возраст это только цифра
+## Ссылка: https://javarush.com/tasks/ru.javarush.python.core.level02.task02
+
+### CodeGym
+## Task: Age is just a number
+## Link: https://codegym.cc/tasks/en.codegym.python.core.level02.task02
+
+### JavaRush
+## Условие:
+# Создайте переменную birth_year и присвойте ей ваш год рождения. Создайте переменную current_year и присвойте ей текущий год. Вычислите ваш возраст и сохраните его в переменную current_age. Выведите ее на экран.
+
+### CodeGym
+## Condition:
+# Create a variable birth_year and assign it your birth year. Calculate your age and save it in the variable current_age. Display it on the screen.
+
+### JavaRush
+## Требования:
+# • Программа должна включать переменную birth_year, которой присваивается целочисленное значение, представляющее год рождения.
+# • Программа должна включать переменную current_year, которой присваивается целочисленное значение, представляющее текущий год.
+# • Программа должна вычислять текущий возраст, вычитая год рождения из текущего года. Это вычисление должно быть сохранено в переменную current_age.
+# • Программа должна использовать функцию print() для вывода значения переменной current_age на экран.
+
+### CodeGym
+## Requirements:
+# • The program should include a variable birth_year that is assigned an integer value representing the year of birth.
+# • The program should include a variable current_year that is assigned an integer value representing the current year.
+# • The program should calculate the current age by subtracting the birth year from the current year. This calculation should be saved in the variable current_age.
+# • The program should use the print() function to display the value of the variable current_age on the screen.
+
+### JavaRush
+## Черновик:
+# # Возраст это только цифра
+#
+# # Создайте переменную birth_year и присвойте ей ваш год рождения.
+# # Создайте переменную current_year и присвойте ей текущий год.
+# # Вычислите ваш возраст и сохраните его в переменную current_age. Выведите ее на экран.
+#
+# # Напишите тут ваш код
+
+### CodeGym
+## Draft:
+# # Age is just a number
+#
+# # Create a variable birth_year and assign it your birth year.
+# # Create a variable current_year and assign it the current year.
+# # Calculate your age and save it in the variable current_age. Display it on the screen.
+#
+# # Write your code here
+
+### JavaRush
+birth_year = 1970
+current_year = 2026
+current_age = current_year - birth_year
+print("Возраст:", current_age)
+
+### CodeGym
+# birth_year = 1970
+# current_year = 2026
+# current_age = current_year - birth_year
+# print("Age:", current_age)
+
+### JavaRush
+## Правильное решение:
+## Author: JavaRush's team
+# birth_year = 1990  # Замените 1990 на ваш реальный год рождения
+# current_year = 2024
+# current_age = current_year - birth_year
+# print("Ваш возраст:", current_age)
+
+### CodeGym
+## Correct solution:
+## Author: CodeGym's team
+# birth_year = 1990  # Replace 1990 with your actual birth year
+# current_year = 2024
+# current_age = current_year - birth_year
+# print("Your age:", current_age)
