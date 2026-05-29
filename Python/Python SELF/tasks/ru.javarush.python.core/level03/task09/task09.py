44aaa0070fa3f8a8d318a0bf3274d779174c90d3
Altavista360 <github@altavista360.email>
2026-05-29 16:37:08 +0300

    ru.javarush.python.core.level03.task09 | en.codegym.python.core.level03.task09

---
+++ Python/Python SELF/tasks/ru.javarush.python.core/level03/task09/task09.py
@@ -0,0 +1,92 @@
+## Authors: JavaRush's team, CodeGym's team, Altavista360
+## Version: 1.0.0
+
+### JavaRush
+## Задача: Комментатор
+## Ссылка: https://javarush.com/tasks/ru.javarush.python.core.level03.task09
+
+### CodeGym
+## Task: Commentator
+## Link: https://codegym.cc/tasks/en.codegym.python.core.level03.task09
+
+### JavaRush
+## Условие:
+# Расставь правильно комментарии.
+
+### CodeGym
+## Condition:
+# Place the comments correctly.
+
+### JavaRush
+## Требования:
+# • Комментарий про "запрашиваем у пользователя его имя" должен относиться к функции input()
+# • Комментарий про "вывод сообщения" должен относиться к функции print()
+
+### CodeGym
+## Requirements:
+# • The comment about "requesting the user's name" should be related to the input() function.
+# • The comment about "displaying the message" should be related to the print() function.
+
+### JavaRush
+## Черновик:
+# # Комментатор
+#
+# # Расставь правильно комментарии.
+#
+# # Выводим приветственное сообщение
+# name = input("Введите ваше имя: ")
+#
+# # Запрашиваем у пользователя его имя
+# print("Привет, " + name + "!")
+
+### CodeGym
+## Draft:
+# # Commentator
+#
+# # Place the comments correctly.
+#
+# # Displaying a greeting message
+# name = input("Enter your name: ")
+#
+# # Requesting the user's name
+# print("Hello, " + name + "!")
+
+### JavaRush
+# Комментатор
+
+# Расставь правильно комментарии.
+
+# Запрашиваем у пользователя его имя
+name = input("Введите ваше имя: ")
+
+# Выводим приветственное сообщение
+print("Привет, " + name + "!")
+
+### CodeGym
+# # Commentator
+#
+# # Place the comments correctly.
+#
+# # Requesting the user's name
+# name = input("Enter your name: ")
+#
+# # Displaying a greeting message
+# print("Hello, " + name + "!")
+
+### JavaRush
+## Правильное решение:
+## Author: JavaRush's team
+# # Запрашиваем у пользователя его имя
+# name = input("Введите ваше имя: ")
+#
+# # Выводим приветственное сообщение
+# print("Привет, " + name + "!")
+
+### CodeGym
+## Correct solution:
+## Author: CodeGym's team
+# # Requesting the user's name
+# name = input("Enter your name: ")
+#
+# # Displaying a greeting message
+# print("Hello, " + name + "!")
