39070ba73ef82c60f7402c5d5f4f1fe7b910727e
Altavista360 <github@altavista360.email>
2026-06-10 07:30:50 +0300

    ru.javarush.python.core.level04.task01 | en.codegym.python.core.level04.task01

---
+++ Python/Python SELF/tasks/ru.javarush.python.core/level04/task01/task01.py
@@ -0,0 +1,55 @@
+## Authors: JavaRush's team, CodeGym's team, Altavista360
+## Version: 1.0.0
+
+### JavaRush
+## Задача: Сила в разнообразии
+## Ссылка: https://javarush.com/tasks/ru.javarush.python.core.level04.task01
+
+### CodeGym
+## Task: Strength in Diversity
+## Link: https://codegym.cc/tasks/en.codegym.python.core.level04.task01
+
+### JavaRush
+## Условие:
+# Создай 6 переменных, чтобы они все хранили значения разного типа.
+
+### JavaRush
+## Требования:
+# • Создай переменную и присвой ей целое число.
+# • Создай переменную и присвой ей дробное (float) число.
+# • Создай переменную и присвой ей строку.
+# • Создай переменную и присвой ей значение типа bool.
+# • Создай переменную и присвой ей список.
+# • Создай переменную и присвой ей словарь.
+
+### JavaRush
+## Черновик:
+# # Сила в разнообразии
+#
+# # Создай 6 переменных, чтобы они все хранили значения разного типа.
+#
+# # Напишите тут ваш код
+
+### JavaRush
+# Сила в разнообразии
+
+# Создай 6 переменных, чтобы они все хранили значения разного типа.
+
+# Напишите тут ваш код
+
+int_var = 27
+float_var = 1.23
+str_var = "String"
+bool_var = True
+list_var = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+dict_var = {"k1": "v1", "k2": "v2", "k3": "v3"}
+
+### JavaRush
+## Правильное решение:
+## Author: JavaRush's team
+# int_var = 42
+# float_var = 3.14
+# str_var = "Hello, world!"
+# bool_var = True
+# list_var = [1, 2, 3, 4, 5]
+# dict_var = {"key1": "value1", "key2": "value2"}
