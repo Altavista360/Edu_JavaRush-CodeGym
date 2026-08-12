b2dd3ee5c34e57a555902d32254cec900af9d8d2
Altavista360 <github@altavista360.email>
2026-08-12 05:51:39 +0300

    ru.javarush.python.core.level10.task01 | en.codegym.python.core.level10.task01

---
+++ Python/Python SELF/tasks/ru.javarush.python.core/level10/task01/task01.py
@@ -0,0 +1,46 @@
+## Authors: JavaRush's team, CodeGym's team, Altavista360
+## Version: 1.0.0
+
+### JavaRush
+## Задача: Деление на ноль.
+## Ссылка: https://javarush.com/tasks/ru.javarush.python.core.level10.task01
+
+### CodeGym
+## Task: Division by Zero.
+## Link: https://codegym.cc/tasks/en.codegym.python.core.level10.task01
+
+### JavaRush
+## Условие:
+# Напиши программу, которая создает исключение ZeroDivisionError
+
+### JavaRush
+## Требования:
+# • Работа программы должна приводить к возникновению исключения ZeroDivisionError.
+
+### JavaRush
+## Черновик:
+# # Деление на ноль.
+#
+# # Напиши программу, которая создает исключение ZeroDivisionError
+#
+# # Напишите тут ваш код
+
+### JavaRush
+# Деление на ноль.
+
+# Напиши программу, которая создает исключение ZeroDivisionError
+
+# Напишите тут ваш код
+
+def create_zero_division_error():
+    return 100 / 0
+
+create_zero_division_error()
+
+### JavaRush
+## Правильное решение:
+## Author: JavaRush's team
+# def create_zero_division_error():
+#     return 1 / 0
+#
+# create_zero_division_error()
