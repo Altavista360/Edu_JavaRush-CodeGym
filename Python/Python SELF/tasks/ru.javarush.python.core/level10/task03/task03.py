dc35ce146a1a32ad1b40d0e1c222328a46484f6f
Altavista360 <github@altavista360.email>
2026-08-13 06:06:20 +0300

    ru.javarush.python.core.level10.task03 | en.codegym.python.core.level10.task03

---
+++ Python/Python SELF/tasks/ru.javarush.python.core/level10/task03/task03.py
@@ -0,0 +1,40 @@
+## Authors: JavaRush's team, CodeGym's team, Altavista360
+## Version: 1.0.0
+
+### JavaRush
+## Задача: Обращение к несуществующей переменной.
+## Ссылка: https://javarush.com/tasks/ru.javarush.python.core.level10.task03
+
+### CodeGym
+## Task: Accessing a non-existent variable.
+## Link: https://codegym.cc/tasks/en.codegym.python.core.level10.task03
+
+### JavaRush
+## Условие:
+# Напиши программу, которая создает исключение IndentationError
+
+### JavaRush
+## Требования:
+# • Программа должна содержать код, вызывающий NameError.
+
+### JavaRush
+## Черновик:
+# # Обращение к несуществующей переменной.
+#
+# # Напиши программу, выполнение которой приводит к исключению NameError.
+#
+# # Напишите тут ваш код
+
+### JavaRush
+# Обращение к несуществующей переменной.
+
+# Напиши программу, выполнение которой приводит к исключению NameError.
+
+# Напишите тут ваш код
+
+print(foobar)
+
+### JavaRush
+## Правильное решение:
+## Author: JavaRush's team
+# print(a)
