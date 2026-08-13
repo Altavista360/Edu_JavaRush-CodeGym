0d98430a13b62c9a7e33a721c0c3e2b9d3fcfb46
Altavista360 <github@altavista360.email>
2026-08-13 06:06:39 +0300

    ru.javarush.python.core.level10.task04 | en.codegym.python.core.level10.task04

---
+++ Python/Python SELF/tasks/ru.javarush.python.core/level10/task04/task04.py
@@ -0,0 +1,41 @@
+## Authors: JavaRush's team, CodeGym's team, Altavista360
+## Version: 1.0.0
+
+### JavaRush
+## Задача: Синтаксическая ошибка.
+## Ссылка: https://javarush.com/tasks/ru.javarush.python.core.level10.task04
+
+### CodeGym
+## Task: Syntax Error.
+## Link: https://codegym.cc/tasks/en.codegym.python.core.level10.task04
+
+### JavaRush
+## Условие:
+# Напиши программу, которая создает исключение SyntaxError
+
+### JavaRush
+## Требования:
+# • Программа должна содержать код, вызывающий SyntaxError.
+
+### JavaRush
+## Черновик:
+# # Синтаксическая ошибка.
+#
+# # Напиши программу, которая создает исключение SyntaxError
+#
+# # Напишите тут ваш код
+
+### JavaRush
+# Синтаксическая ошибка.
+
+# Напиши программу, которая создает исключение SyntaxError
+
+# Напишите тут ваш код
+
+raise SyntaxError("foobar")
+
+### JavaRush
+## Правильное решение:
+## Author: JavaRush's team
+# # Создаем исключение SyntaxError
+# raise SyntaxError("Это синтаксическая ошибка")
